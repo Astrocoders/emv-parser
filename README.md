14f37e740863a4c2bf72e27f37ddfb772ca33879
Gabriel R. Abreu <grsabreu@gmail.com>
2020-09-04 15:09:48 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# cielo-qrcode-parser
+
+npm install cielo-qrcode-parser
+
+```js
+const {treeToObject, parser} = require('./index')
+
+const emv = `00020101021226410005Cielo0116123456789012000102082009130352040000530398654120000000001005802BR5905CIELO6014SANTO ANDRE SP801010033"https://www.cielo.com.br/qrcode"011613050329197F190A0212150518113349030410000404000105020006020163049872`
+
+console.log(treeToObject(parser.run(emv).result))
+// =>
+{
+  payloadFormatIndicator: '01',
+  pointOfInitiationMethod: '12',
+  merchantAccountInformation: {
+    globallyUniqueIdentifier: 'Cielo',
+    merchantAccountInformation: '1234567890120001',
+    logicNumber: '20091303'
+  },
+  merchantCategory: '0000',
+  transactionCurrency: '986',
+  transactionAmount: '000000000100',
+  countryCode: 'BR',
+  merchantName: 'CIELO',
+  merchantCity: 'SANTO ANDRE SP',
+  transactionInformations: {
+    globallyUniqueIdentifier: '"https://www.cielo.com.br/qrcode"',
+    transactionId: '13050329197F190A',
+    transactionDate: '150518113349',
+    mainProduct: '1000',
+    subProduct: '0001',
+    paymentInstallments: '00',
+    transactionType: '01'
+  },
+  crc: '9872'
+}
+```
---
+++ index.js
@@ -0,0 +1,84 @@
+const A = require('arcsecond')
+
+const tlv = A.recursiveParser(() => A.coroutine(function* () {
+  const id = yield A.sequenceOf([
+    A.digit,
+    A.digit,
+  ]).map(v => v.join(''));
+
+  if(id == '80') {
+    const length = yield A.sequenceOf([
+      A.digit,
+      A.digit,
+      A.digit,
+    ]).map(v => v.join(''));
+
+    const transactionInformations = yield A.sequenceOf(
+      Array.from({length: Number(length)}).map(() => A.anyChar)
+    ).map(v => v.join(''))
+
+    return {
+      id,
+      length,
+      value: parser.run(transactionInformations)
+    }
+  }
+
+  const length = yield A.sequenceOf([
+    A.digit,
+    A.digit,
+  ]).map(v => v.join(''));
+
+  const value = yield A.sequenceOf(
+    Array.from({length: Number(length)}).map(() => A.anyChar)
+  ).map(v => v.join(''))
+
+
+  if(id === '26') {
+    return {id, length, value: parser.run(value)}
+  }
+
+  return {id, length, value}
+}));
+
+const parser = A.many(tlv)
+
+const getIdValue = (id, tree) => {
+  const field = tree.find(field => field.id === id)
+
+  return field ? field.value : null
+}
+
+const treeToObject = tree => {
+  const merchantAccountInformation = getIdValue("26", tree).result;
+  const transactionInformations = getIdValue("80", tree).result;
+
+  return {
+      payloadFormatIndicator: getIdValue('00', tree),
+      pointOfInitiationMethod: getIdValue("01", tree),
+      merchantAccountInformation: {
+        globallyUniqueIdentifier: getIdValue("00", merchantAccountInformation),
+        merchantAccountInformation: getIdValue("01", merchantAccountInformation),
+        logicNumber: getIdValue("02", merchantAccountInformation),
+      },
+      merchantCategory: getIdValue("52", tree),
+      transactionCurrency: getIdValue("53", tree),
+      transactionAmount: getIdValue("54", tree),
+      countryCode: getIdValue("58", tree),
+      merchantName: getIdValue("59", tree),
+      merchantCity: getIdValue("60", tree),
+      transactionInformations: {
+        globallyUniqueIdentifier: getIdValue("00", transactionInformations),
+        transactionId: getIdValue("01", transactionInformations),
+        transactionDate: getIdValue("02", transactionInformations),
+        mainProduct: getIdValue("03", transactionInformations),
+        subProduct: getIdValue("04", transactionInformations),
+        paymentInstallments: getIdValue("05", transactionInformations),
+        transactionType: getIdValue("06", transactionInformations),
+      },
+      crc: getIdValue("63", tree),
+  };
+};
+
+module.exports.parser = parser
+module.exports.treeToObject = treeToObject
